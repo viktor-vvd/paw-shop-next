967d36c5d9acdd822822011cbb6837154004e27b
viktor-vvd <davidenko3015@gmail.com>
2023-10-24 14:34:10 +0300

    fix

--- components/DefaultLayout.jsx
+++ components/DefaultLayout.jsx
@@ -1,9 +1,9 @@
 import Footer from "@components/base/Footer";
 import Header from "@components/base/Header/Header";
 import React from "react";
+
 import { Montserrat_Alternates, Montserrat } from 'next/font/google';
 import localFont from 'next/font/local';
-
 export const montserrat_alternates = Montserrat_Alternates({
   weight: ['300', '400', '500', '700', '900'],
   style: ['normal'],
@@ -11,12 +11,13 @@ export const montserrat_alternates = Montserrat_Alternates({
   display: 'swap',
   variable: '--font-montserrat_alternates',
 });
-// export const amatic_sc = Amatic_SC({
-//   weight: ['700'],
-//   style: ['normal'],
-//   subsets: ['latin'],
-//   display: 'swap',
-// });
+export const montserrat = Montserrat({
+  weight: ['300', '400', '500', '700', '900'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat_alternates',
+});
 export const amatic_sc = localFont({
   src: [
     {
@@ -26,20 +27,24 @@ export const amatic_sc = localFont({
   ],
 });
 
-export const montserrat = Montserrat({
-  weight: ['500'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
+export const inter = localFont({
+  src: [
+    {
+      path: './../src/assets/fonts/Inter/Inter-VariableFont_slnt,wght.ttf',
+      weight: '400',
+    },
+  ],
 });
+
 const DefaultLayout = ({ children }) => {
   return (
     <div className="App">
       <style jsx global>{`
         :root {
           --font-montserrat_alternates: ${montserrat_alternates.style.fontFamily};
-          --font-amatic_sc: ${amatic_sc.style.fontFamily};
           --font-montserrat: ${montserrat.style.fontFamily};
+          --font-amatic_sc: ${amatic_sc.style.fontFamily};
+          --font-inter: ${inter.style.fontFamily};
         }
       `}</style>
     <Header />
